9dbebb4090ede9b71ee237f2204b8bbc7b9d99b7
Luiz Fellype <lfcassago@gmail.com>
2020-01-15 22:58:27 -0300

    feat: connect db and start server

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+const {PORT} = require('./constants')
+
+mongoose.connect('mongodb+srv://devsearch:devsearch@cluster0-bmyia.mongodb.net/omnistack10?retryWrites=true&w=majority' ,{
+    useCreateIndex: true,    
+useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const app = express()
+
+
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(PORT, ()=> console.log(`Funfando Parceiro, So vai pra http://localhost:${PORT}/`))
