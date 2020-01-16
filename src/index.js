47282a4c2a1e04034c8b268aed2ee352f9b6b2b6
Luiz Fellype <lfcassago@gmail.com>
2020-01-16 00:04:37 -0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 const routes = require('./routes')
 const {PORT} = require('./constants')
 
 mongoose.connect('mongodb+srv://devsearch:devsearch@cluster0-bmyia.mongodb.net/omnistack10?retryWrites=true&w=majority' ,{
     useCreateIndex: true,    
-useNewUrlParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
 const app = express()
+
+app.use(cors())
 
 
 app.use(express.json())
